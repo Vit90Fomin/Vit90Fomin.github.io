58cd3ddd1ad534b9f7b9d778f2179c45354566a7
Vit90Fomin <116459411+Vit90Fomin@users.noreply.github.com>
2022-10-23 01:03:50 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Carousel
+const owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+  items: 3,
+});
+
+$(".slider__btn--prev").click(function () {
+  owl.trigger("prev.owl.carousel");
+});
+
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+
+// Nav icon
+const navBtn = document.querySelector(".nav__toggle");
+const menuIcon = document.querySelector(".menu-icon");
+
+navBtn.onclick = function () {
+  menuIcon.classList.toggle("menu-icon--active");
+};
